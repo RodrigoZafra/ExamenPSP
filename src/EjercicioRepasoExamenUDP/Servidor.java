f5afb66a54e49dd6545e430f4ba2ae92878b4d17
am3dam2 <rodrifdt17@gmail.com>
2022-02-18 12:03:45 +0100

    terminando servidor

--- src/EjercicioRepasoExamenUDP/Servidor.java
+++ src/EjercicioRepasoExamenUDP/Servidor.java
@@ -13,6 +13,7 @@ public class Servidor {
         byte[] buffer = new byte[1024];
         String palabra = "Perro";
         DatagramPacket paqueteCliente = null;
+        DatagramPacket paqueteServer = null;
         InetAddress address = null;
         int puertoCliente;
         String mensaje = "";
@@ -30,13 +31,20 @@ public class Servidor {
                 buffer = new byte[1024];
                 longitud = String.valueOf(palabra.length());
                 buffer = longitud.getBytes();
-                DatagramPacket paqueteServer = new DatagramPacket(buffer, buffer.length, address, puertoCliente);
+                paqueteServer = new DatagramPacket(buffer, buffer.length, address, puertoCliente);
                 sk.send(paqueteServer);
 
                 for (int i = 0; i < palabra.length(); i++) {
                     paqueteCliente = new DatagramPacket(buffer, buffer.length);
                     sk.receive(paqueteCliente);
                     mensaje = new String(paqueteCliente.getData());
+                    if (!mensaje.equalsIgnoreCase(palabra)) {
+                        buffer = new byte[1024];
+                        longitud = String.valueOf(palabra.charAt(i));
+                        buffer = longitud.getBytes();
+                        paqueteServer = new DatagramPacket(buffer, buffer.length, address, puertoCliente);
+                        sk.send(paqueteServer);
+                    }
                 }
             }
         } catch (SocketException e) {
